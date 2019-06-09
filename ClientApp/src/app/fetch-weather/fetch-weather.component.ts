6a258832b2a15ba7ec8520cf41e3810bf81fbd3a
Tom <tom@Toms-MacBook-Pro.local>
2019-06-10 10:02:34 +1200

    Initial Commit

---
+++ ClientApp/src/app/fetch-weather/fetch-weather.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../../node_modules/rxjs';
+
+@Component({
+  selector: 'app-fetch-weather',
+  templateUrl: './fetch-weather.component.html'
+})
+    export class FetchWeatherComponent{
+
+   
+
+ public forecasts;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+      http.get(baseUrl + 'api/WeatherData/WeatherForecasts')
+          .subscribe(result => {
+        this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+
---
+++ Controllers/WeatherDataController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherDataController : Controller
+    {
+         
+
+        public WeatherDataController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        [HttpGet]
+        [Route("WeatherForecasts")]
+        public async Task<ActionResult<string>> WeatherForecasts()
+        {
+            var client = _httpClientFactory.CreateClient("OpenWeatherMap");
+            var maxRetryAttempts = 3;
+            var pauseBetweenFailures = TimeSpan.FromSeconds(2);
+
+            var retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(maxRetryAttempts, i => pauseBetweenFailures);
+
+            var result = "";
+            var retries = 0;
+            await retryPolicy.ExecuteAsync(async () =>
+            {
+                result = await client.GetStringAsync("");
+                Console.WriteLine("Weather API request attempt: " + retries + "/" + maxRetryAttempts);
+            });
+            return result;
+        }
+    }
+}
